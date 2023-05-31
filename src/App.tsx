bdf3ca5d813023a37db4bf40c9d122ce41de2400
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-05-31 15:14:30 +0200

    Coding a test for POC

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 
 // Import style
 import './assets/style/css/style.css'
+import {useEffect, useState} from "react";
+
+interface squareInt {
+  row: string,
+  col: number
+}
 
 function App() {
 
+  const [myNumber, setMyNumber] = useState(0)
+  const [myText, setMyText] = useState("")
+  const [squareId, setSquareId] = useState<squareInt[]>([])
+
+  const saveSquare = () => {
+    setSquareId([...squareId, { row: myText, col: myNumber }])
+  }
+
+  useEffect(() => {
+    console.log(squareId)
+  }, [squareId])
+
   return (
     <div>
-
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        saveSquare()
+      }}>
+        <input type="number" value={myNumber} onChange={(e) => setMyNumber(parseInt(e.target.value))}/>
+        <input type="text" value={myText} onChange={(e) => setMyText(e.target.value)} />
+        <button type="submit">Check</button>
+      </form>
     </div>
   )
 }
