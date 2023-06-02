b671424d6792759aae2007742cf853669e727811
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-06-02 09:37:59 +0200

    Switching between players and update the game field

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
 
     const [gameGrid, setGameGrid] = useState<GameGridInt[]>([])
     const [playedGrid, setPlayedGrid] = useState<GameGridInt[]>([])
+    const [player, setPlayer] = useState ("userA")
 
     useEffect(() => {
         setGameGrid(data)
@@ -30,16 +31,38 @@ function App() {
             user: user
         }])
 
-        // Update gameGrid with new game data
-        setGameGrid(id, {
-            user: "userA"
-        })
+        // Update gameGrid with new game data and switch player
+        updateGameGrid(user, id)
+        switchPlayer()
+    }
+
+    // Switch player
+    const switchPlayer = () => {
+        if(player === "userA") {
+            setPlayer("userB")
+        } else {
+            setPlayer("userA")
+        }
+    }
+
+    const updateGameGrid = (user:string, id:number) => {
+        const updateGrid = [...gameGrid]
+        const targetGrid = updateGrid.find(grid => grid.id === id)
+
+        if(targetGrid) {
+            targetGrid.user = user
+        }
+
+        console.log({player})
+
+        setGameGrid(updateGrid)
+        console.log({gameGrid})
     }
 
     return (
         <div className="parent">
             {gameGrid && gameGrid.map(div => (
-                <div onClick={() => handleClick(div.user, div.col, div.row, div.id)} className={`checked ${div.user}`} data-col={div.col} data-id={div.id}  data-row={div.row} >{div.col} {div.row}</div>
+                <div key={div.id} onClick={() => handleClick(player, div.col, div.row, div.id)} className={`checked ${div.user}`} data-col={div.col} data-id={div.id}  data-row={div.row} >{div.col} {div.row}</div>
             ))}
         </div>
     )
