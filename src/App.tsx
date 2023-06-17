fa03973ce0b2a142dde6338ed0d383d503a01ffa
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-06-17 09:50:40 +0200

    Logic for two players and game board is in first version and works

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,15 @@ interface GameGridInt {
     row: string,
     checked?: boolean,
     user: string
+    line?: string
 }
 
 interface GameFieldInt {
     grid: string,
     times: number,
-    user: string
+    lines: number,
+    user: string,
+    line?: string|undefined
 }
 
 function App() {
@@ -24,35 +27,62 @@ function App() {
     const [playedGrid, setPlayedGrid] = useState<GameGridInt[]>([])
     const [player, setPlayer] = useState ("userA")
 
+    const [isLine, setIsLine] = useState(false)
+    const [isMiddle, setIsMiddle] = useState(false)
+
     const gameField:GameFieldInt[] = [
         {
             grid: "A",
             times: 0,
+            lines: 0,
             user: ""
         },
         {
             grid: "B",
             times: 0,
+            lines: 0,
             user: ""
         },
         {
             grid: "C",
             times: 0,
+            lines: 0,
             user: ""
         },
         {
             grid: "D",
             times: 0,
+            lines: 0,
             user: ""
         },
         {
             grid: "E",
             times: 0,
+            lines: 0,
             user: ""
         },
         {
             grid: "F",
             times: 0,
+            lines: 0,
+            user: ""
+        },
+        {
+            grid: "RIGHT",
+            times: 0,
+            lines: 0,
+            user: ""
+        },
+        {
+            grid: "LEFT",
+            times: 0,
+            lines: 0,
+            user: ""
+        },
+        {
+            grid: "MIDDLE",
+            times: 0,
+            lines: 0,
             user: ""
         },
     ]
@@ -72,27 +102,39 @@ function App() {
                         return grid
                     }
                 }).filter(Boolean)
-                console.log({playerGrids})
+
                 // Check the result
-                let lineCounter = 0
+                const lineCounter:string[] = []
                 gameField.forEach(letter => {
                     playerGrids.map(grid => {
-                        if(grid && letter.grid === grid.col || grid && letter.grid === grid.row) {
-                            letter.times += 1
+                        if(grid && letter.grid === grid.col || grid && letter.grid === grid.row || grid && letter.grid === grid.line) {
+                            letter.times +=1
                             letter.user = player
-                        }
-                        if(letter.times >= 1) {
-                            lineCounter +=1;
-                            console.log({lineCounter})
+                            if(grid.line === "LEFT" || grid.line === "RIGHT" || grid.line === "MIDDLE") {
+                                letter.lines += 1
+                                letter.user = player
+                            }
                         }
                     })
-                    if(lineCounter === 12) {
-                        console.log("Yey, player: ", letter.user, " Won")
-                    }
                     if(letter.times === 3) {
-                        console.log("Yey, player: ", letter.user, " Won")
+                        console.log("Done times")
+                        setIsLine(true)
+                        return
                     }
+                    const checkLine:string[] = ['LEFT', 'RIGHT']
+                    if(letter.grid === 'MIDDLE' && letter.times === 1) {
+                        console.log("setIsMiddle????")
+                        setIsMiddle(true)
+                    }
+                    checkLine.forEach(item => {
+                        if(letter.grid === item && letter.lines === 2) {
+                            console.log("setIsLine????")
+                            setIsLine(true)
+                        }
+                    })
                 })
+                console.log({lineCounter})
+                console.log({playerGrids})
                 console.log({gameField})
                 // Call for switching players
                 switchPlayers()
@@ -101,6 +143,12 @@ function App() {
         // Call to check the players results with played grids
         checkGridResults()
     }, [playedGrid])
+
+    useEffect(() => {
+        if(isLine && isMiddle) {
+            console.log("GAME COMPLETE, user:")
+        }
+    }, [isMiddle, isLine])
 
     // Switch players
     const switchPlayers = () => {
@@ -111,24 +159,25 @@ function App() {
         }
     }
 
-    const handleClick = (user:string, col:string, row:string, id:number, checked:boolean|undefined) => {
+    const handleClick = (user:string, col:string, row:string, id:number, checked:boolean|undefined, line:string|undefined) => {
         if(checked) {
             return
         }
         // Call Save played grids
-        savePlayedGrid(user, col, row, id)
+        savePlayedGrid(user, col, row, id, line)
         // Call Update gameGrid with new game data
         updateGameGrid(id)
     }
 
     // Save played grids
-    const savePlayedGrid = (user:string, col:string, row: string, id: number) => {
+    const savePlayedGrid = (user:string, col:string, row: string, id: number, line:string|undefined) => {
         // Save played grids in new array that checks the game grid score
         const newPlayed:GameGridInt[] = [...playedGrid, {
             id: id,
             col: col,
             row: row,
-            user: user
+            user: user,
+            line: line
         }]
         setPlayedGrid(newPlayed)
         // Call check grid results
@@ -153,7 +202,7 @@ function App() {
         <>
             <div className="parent">
                 {gameGrid && gameGrid.map(div => (
-                    <div key={div.id} onClick={() => handleClick(player, div.col, div.row, div.id, div.checked)} className={`checked ${div.user}`} data-col={div.col} data-id={div.id}  data-row={div.row} >{div.col} {div.row}</div>
+                    <div key={div.id} onClick={() => handleClick(player, div.col, div.row, div.id, div.checked, div.line)} className={`checked ${div.user}`}>{div.col} {div.row}</div>
                 ))}
             </div>
             <div>{player}</div>
